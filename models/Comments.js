d99c1e26f04bfa0d022c8af2659fda4541ae78b7
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-24 11:57:51 +0800

    creating comments model

---
+++ models/Comments.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const BlogPost = require('./BlogPost');
+
+// DESCRIPTION: Initialize Comments model (table) by extending off Sequelize's Model class
+class Comments extends Model {}
+
+// DESCRIPTION: set up fields and rules for Comments model
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        blogPost_id:{
+            type: DataTypes.INTEGER,
+            references:{ 
+              model: BlogPost,
+              key: 'id',
+            //   unique: false FIXME: confirm
+            }
+        }
+    }
+);
+
+module.exports = Comments;
