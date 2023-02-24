5360392abdb670346ab854d2b636687aeb544098
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-24 11:58:12 +0800

    adding comments model relationships

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
+const Comments = require('./Comments');
 
 // users can have multiple blog posts
 User.hasMany(BlogPost, {
@@ -10,6 +11,17 @@ User.hasMany(BlogPost, {
 // blog post belongs to one user
 BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+// blog post can have multiple comments
+BlogPost.hasMany(Comments, {
+  foreignKey: 'blogPost_id',
+  onDelete: 'CASCADE'
+});
+
+// comments belongs to one blog post
+Comments.belongsTo(BlogPost, {
+  foreignKey: 'blogPost_id'
 });
 
 module.exports = { User, BlogPost };
