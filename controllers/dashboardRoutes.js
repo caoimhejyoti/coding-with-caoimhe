9ea15c699dfad051a4f5ac52efd08732c2483ed0
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-28 18:23:36 +0800

    adding initital update code

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -6,8 +6,7 @@ router.get('/', async (req, res) => {
   try {
     // Get all BlogPosts and JOIN with user data
     const BlogPostData = await BlogPost.findAll({
-      where:
-      {user_id: req.session.user_id},
+      where: { user_id: req.session.user_id },
       include: [
         {
           model: User,
@@ -17,26 +16,29 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const BlogPosts = BlogPostData.map((BlogPost) => BlogPost.get({ plain: true }));
+    const BlogPosts = BlogPostData.map((BlogPost) =>
+      BlogPost.get({ plain: true })
+    );
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      BlogPosts, 
-      logged_in: req.session.logged_in 
+    res.render('homepage', {
+      BlogPosts,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//COMPLETE! DESCRIPTION: Creating a new blog post
 router.post('/', withAuth, async (req, res) => {
   try {
     const newUserPost = await BlogPost.create({
-        user_id: req.session.user_id,
-        title: req.body.name,
-        blog_text: req.body.content,
-        date: new Date,
-      });
+      user_id: req.session.user_id,
+      title: req.body.name,
+      blog_text: req.body.content,
+      date: new Date(),
+    });
 
     console.log(newUserPost);
 
@@ -46,6 +48,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+//COMPLETE! DESCRIPTION: Deleting specific blog post with ID
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const chosenId = req.params.id;
@@ -62,9 +65,26 @@ router.delete('/:id', withAuth, async (req, res) => {
 
     deletedBlogPost;
     res.status(200).json({ message: 'Blog Post successfully deleted.' });
-
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+//DESCRIPTION: updating specific blog post with ID
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await BlogPost.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updatedPost[0]) {
+      res.status(404).json({ message: 'No Category with this id!' });
+      return;
+    }
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
