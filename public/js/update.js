43d5d52a1641c5536f238a55f4bed421d40488b8
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-01 20:53:17 +0800

    working on put request to update blog. buggy!

--- public/js/update.js
+++ public/js/update.js
@@ -2,24 +2,36 @@ console.log('hello - in update.js'); //used for debugging
 
 //DESCRIPTION: ASYNC FNC to update a blog post using the specified id.
 const updateBtnHandler = async (event) => {
-    //How is the content being created?
-    const name = document.querySelector('#update-blogPost-name').value.trim();
-    const content = document
-      .querySelector('#update-blogPost-content')
-      .value.trim();
-  
-    try {
-      const id = event.target.getAttribute('data-id');
-      console.log(id);
-      const response = await fetch(`/dashboard/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ name, content }),
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      }
-    } catch (err) {
-      alert('Failed to delete Post');
+  event.preventDefault();
+  console.log('hellow - in update fnc'); //used for debugging
+
+  const updatedName = document
+    .querySelector('#update-blogPost-name')
+    .value.trim();
+  const updatedContent = document
+    .querySelector('#update-blogPost-content')
+    .value.trim();
+
+  try {
+    console.log(updatedName); //used for debugging
+    console.log(updatedContent); //used for debugging
+    const id = event.target.getAttribute('data-id');
+    console.log(id);
+    const response = await fetch(`/api/updatepost/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ updatedName, updatedContent }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // console.log(response);
+
+    if (response.ok) {
+      console.log('worked');
+      // document.location.replace('/dashboard');
     }
-  };
+  } catch (err) {
+    console.log(err); //used for debugging
+    alert('Failed to update Post');
+  }
+};
+
+document.querySelector('#submit').addEventListener('click', updateBtnHandler);
