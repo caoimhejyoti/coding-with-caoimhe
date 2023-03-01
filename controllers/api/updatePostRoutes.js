6508c782e8098f68dcd9959583e3e1a8c92267e1
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-01 19:35:06 +0800

    getting the post to display

--- controllers/api/updatePostRoutes.js
+++ controllers/api/updatePostRoutes.js
@@ -3,32 +3,36 @@ const { Model } = require('sequelize');
 const { BlogPost } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
 // DESCRIPTION: get one blog post from id
-router.get('/updatepost/:id', withAuth, async (req, res) => {
-    // find one blog post by its `id` value
-    try{
-      const blogPostData = await BlogPost.findByPk(req.params.id);
-  
-      if(!blogPostData){
-        res.status(404).json({message: 'No Blog found with that id'});
-        return;
-      }
-      
-      res.render('updatepost', {
-        blogPostData,
-        logged_in: req.session.logged_in
-      });
-    }catch (err){
-      res.status(500).json(err);
+router.get('/:id', withAuth, async (req, res) => {
+  // find one blog post by its `id` value
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id);
+    console.log('hello');
+    console.log(blogPostData);
+    if (!blogPostData) {
+      res.status(404).json({ message: 'No Blog found with that id' });
+      return;
     }
+
+    const BlogPostInfo = blogPostData.get({ plain: true });
+
+    res.render('updatepost', {
+      BlogPostInfo,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // // DESCRIPTION: update a blog post by its `id` value
-router.put('/updatepost/:id', withAuth, async (req, res) => {
-    try {
-      console.log('Hello in get function (update.js)'); //use for debugging
-      const BlogPostData = await BlogPost.findByPk(req.params.id, {
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    console.log('Hello in get function (update.js)'); //use for debugging
+    const BlogPostData = await BlogPost.findByPk(
+      req.params.id,
+      {
         include: [
           {
             model: User,
@@ -36,18 +40,18 @@ router.put('/updatepost/:id', withAuth, async (req, res) => {
           },
         ],
       },
-      console.log(BlogPostData),
-      );
-      console.log(BlogPostData);
-      const BlogPostInfo = BlogPostData.get({ plain: true });
-  
-      res.render('BlogPost', {
-        ...BlogPostInfo,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
+      console.log(BlogPostData)
+    );
+    console.log(BlogPostData);
+    const BlogPostInfo = BlogPostData.get({ plain: true });
+
+    res.render('BlogPost', {
+      ...BlogPostInfo,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
