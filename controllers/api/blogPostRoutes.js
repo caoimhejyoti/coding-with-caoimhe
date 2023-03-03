56b526a6d1a76d5e0d1d754078818729784a28ad
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-03 08:40:25 +0800

    removing dead code

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,8 +1,3 @@
-// WHEN I click on an existing blog post
-// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
-// WHEN I enter a comment and click on the submit button while signed in
-// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
-
 const router = require('express').Router();
 const { BlogPost, Comments } = require('../../models');
 const withAuth = require('../../utils/auth');
@@ -29,55 +24,5 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-
-
-// //DESCRIPTION: Creating a new blog comment
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newComment = await Comments.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//       Date: new Date
-//     });
-
-//     res.status(200).json(newComment);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-// // DESCRIPTION: Creating a new comment
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     console.log('inside comment POST request');
-//     const id = req.session.user_id;
-//     const newComment = await Comments.create(
-//       {
-//         comment: req.body.comment,
-//         blogPost_id: req.body.post_id,
-//       },
-//       {
-//         where: {
-//           user_id: id,
-//         },
-//       }
-//     );
-
-//     // console.log('new comment:');
-//     // console.log(newComment);
-//     const userComment = newComment.get({ plain: true });
-
-//     res.render('blogpost', {
-//       userComment,
-//       logged_in: req.session.logged_in,
-//     });
-
-//     res.status(200).json(userComment);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(400).json(err);
-//   }
-// });
 
 module.exports = router;
