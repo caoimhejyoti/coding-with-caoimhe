c0f294622e99ddacd4ae3cb83c6f69bfe534ed7a
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-05 18:15:06 +0800

    tidying code

--- controllers/api/updatePostRoutes.js
+++ controllers/api/updatePostRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// const { Model } = require('sequelize');
 const { BlogPost } = require('../../models');
 const withAuth = require('../../utils/auth');
 
